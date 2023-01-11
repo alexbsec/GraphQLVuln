c583d7ddc335b3bd3c2bea6a8199c7c6fa16a066
Joao Buschinelli <j.a.c.buschinelli@gmail.com>
2023-01-11 00:50:56 -0300

    Modified the architecture of the package

--- Graphql.py
+++ Graphql.py
@@ -1,79 +1,104 @@
 
 import json
+import requests
+from requests import *
+from bs4 import BeautifulSoup
+import time
 
 
-class GraphqlQuery:
-    def __init__(self, induce_error=False,*args, **kwargs):
-        # Analyzing what kind of arguments are passed to 
-        # be parsed into a valid query.
+LETTERS = 'abcdefghijklmnopqrs'
+
+
+class NormalQuery:
+    def __init__(self, url, endpoint, method, induce_error=False, req_per_sec=10, timout=20, **kwargs):
+        self.URL = url
+
+
+class IntrospectionQuery:
+    def __init__(self, url, endpoint, method, induce_error=False, req_per_sec=10, timeout=20, **kwargs):
+        self.URL = url
+        self.endpoint = endpoint
         self.induce_error = induce_error
-        key_types = ["Type of query input",
-        "Induce error query"
-        ]
-        self.inputs = {}
-        self.args = args
-        self.kwargs = kwargs
+        self.requests_per_second = req_per_sec
+        self.timout = timeout
 
-        interpreter = Interpreter(args, kwargs)
-
-
-
-    def json_query(self, json_file):
-        self.
-
-
-class Interpreter:
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-        for kwarg in kwargs:
-            query_type = self.identifier(kwarg)
-
-
-
-    def identifier(self, query):
-        if type(query) == dict:
-            return "JSON"
-        elif type(query) == str:
-            self.match_reader(query)
-
-
-
-    def match_reader(self, string):
-        first_patterns = ['query'
+        self.query_db = [
+            "{__schema}",
+            "{__schema}",
+            "{__schema{types}}",
+            "{__schema{types{names}}}",
+            "{__schema{queryType{name}}}"
         ]
 
-        string = string.replace(' ', '')
+        self.invalid_query_db = [
+            "{__asdohk}",
+            "{__schema{thisisnotavalidquery}}"
+        ]
 
-        if '\n' in string:
-            string = string.replace('\n', '')
+        self.error_responses = [
+            "Cannot query field",
+            "GraphQL introspection is not allowed"
+        ]
 
+        soups_responses = self.request_query(method)
+        self.soup_analyzer(soups_responses)
 
-        if string[0] != '{':
-            for i in range(len(first_patterns[0])):
-                pattern = first_patterns[0]
-                if pattern[i] != string[i]:
-                    raise MismatchError('string', pattern, string, i)
-
-
-        for char in string:
-            print('o')
-
+        
 
 
 
+    def request_query(self, method, **kwargs):
+        soups = {}
 
-class MismatchError:
+        if not self.induce_error:
+            for query in self.query_db:
+                print(f'[-] Applying {self.method} request on {self.URL} with query {query}...')
+                r = requests.method(self.URL, data={"query":query}, timout=self.timout)
+                soup = BeautifulSoup(r.content, 'html.parser')
+                soups[query] = soup
+                time.sleep(self.requests_per_second)
 
-    def __init__(self, what_type, expected, given, location):
-        self.location = location
-        self.expected = expected
-        self.given = given
+            return soups
 
-        if what_type == 'string':
-            self.what_type = what_type
-            self.string_mismatch()
+        for query in self.invalid_query_db:
+            print(f'[-] Applying {self.method} request on {self.URL} with invalid query {query}...')
+            r = requests.method(self.URL, data={"query":query})
+            soup = BeautifulSoup(r.content, 'html.parser')
+            soups[query] = soup
+            time.sleep(self.requests_per_second)
+
+        return soups
+        
+            
+    def soup_analyzer(self, responses):
+        keys = list(responses)
+        self.errors = {}
+        self.accepted = {}
+        for query in keys:
+            for error in self.error_responses:
+                if error in responses[query]:
+                    print(f"[+] Endpoint GraphQL returned an error {responses[query]}.")
+                    self.errors[query] = responses[query]
+                    print("[-] Analyzing possible vulnerabilities...")
+                    self.error_breaches(responses[query])
+                else:
+                    print(f"[+] Query {responses[query]} received a response that could lead to information disclosure, sqli and CRSF.")
+                    self.accepted[query] = responses[query]
 
 
-    def string_mismatch(self):
-        print(f'MismatchError: Expected query does not match the one provided. Expected {self.expected[self.location]}, got {self.given[self.locatio]} in char [{self.location}.\n Type of query: {self.what_type}')
+        
+
+    def error_breaches(self, error):
+        if self.error_responses[1] in error:
+            print("[!] Introspection is disabled.")
+            print("[-] Try to enumerate fields with normal queries.")
+            
+
+
+
+
+
+
+        
+
+
